933773bdb72b7fdb815391435678203f24394d4a
Kamlesh Yadav <kamyadav1994@gmail.com>
2025-03-16 07:58:23 +0530

    fix

--- infra/infra_stack.py
+++ infra/infra_stack.py
@@ -39,7 +39,8 @@ class InfraStack(Stack):
             cluster_name="DevOpsCluster",
             vpc=vpc,
             version=eks.KubernetesVersion.V1_30,
-            default_capacity=0  # Required for CDK 2.150.0
+            default_capacity=0
+            kubectl_layer=kubectl_layer  # Required for CDK 2.150.0
         )
 
         # Add managed node group
